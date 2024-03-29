e8f2c710fd6d6b43ff94ee17492d94ea9b53c646
Jianmin Chen <59099858+jianmin-chen@users.noreply.github.com>
2024-03-29 14:58:33 -0400

    Final for now

--- src/lib/bag.ts
+++ src/lib/bag.ts
@@ -4,8 +4,7 @@ import { trim } from './utils'
 const bagClient = async () =>
   await App.connect({
     appId: Number(import.meta.env.BAG_APP_ID),
-    key: import.meta.env.BAG_APP_KEY,
-    baseUrl: 'http://0.0.0.0:3000'
+    key: import.meta.env.BAG_APP_KEY
   })
 
 const filter = async (recipes: Recipe[], items: Item[]): Promise<Item[]> => {
@@ -53,8 +52,55 @@ declare global {
 }
 
 export const bag = globalThis.bag ?? (await bagClient())
+const invalidItems = [
+  'gp',
+  'Dinoisseur Challenge Coin',
+  'Sockmeister Challenge Coin',
+  'Wheat Seeds',
+  'Grass Seeds',
+  'Coconut',
+  'Wheat Seeds',
+  'Grass Seeds',
+  'Fish Hat',
+  'Fish',
+  '1982 Wurlitzer Jukebox',
+  'MASH Season 1',
+  'MASH Season 2',
+  'MASH Season 3',
+  'MASH Season 4',
+  'MASH Season 5',
+  'MASH Season 6',
+  'MASH Season 7',
+  'MASH Season 8',
+  'MASH Season 9',
+  'MASH Season 10',
+  'MASH Season 11',
+  'MASH The Good Seasons',
+  'MASH The Complete Series',
+  'AfterMASH Season 1',
+  'AfterMASH Season 2',
+  'AfterMASH The Complete Series',
+  'MASH The Movie',
+  'MASH The Original Novel',
+  'MASH The Ultimate Collection',
+  'Bonsai',
+  "Kamina's Shades",
+  'Blahaj',
+  'Cotton',
+  'Hairball',
+  'Cat Hat',
+  'Acorn',
+  'Rice',
+  'Crab',
+  'Clam',
+  'Cheese'
+]
 export const items =
-  globalThis.items ?? (await bag.getItems({ query: JSON.stringify({}) }))
+  globalThis.items ??
+  (await bag.getItems({ query: JSON.stringify({}) })).filter(item => {
+    if (invalidItems.includes(item.name)) return false
+    return true
+  })
 export const recipes = globalThis.recipes ?? (await getRecipes())
 export const canBeCrafted =
-  globalThis.canBeCrafted ?? (await filter(recipes, items))
+  globalThis.canBeCrafted ?? (await filter(recipes as Recipe[], items))
